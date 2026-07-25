25edc5abd09df142756b751b160340886064eb90
kwetu-stack <kwetudist@gmail.com>
2026-07-25 09:21:37 +0300

    Prepare Railway PostgreSQL deployment

--- config.py
+++ config.py
@@ -6,8 +6,13 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "coast-insights-dev-key")
 
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "coast_insights.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "instance", "coast_insights.db"),
     )
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
